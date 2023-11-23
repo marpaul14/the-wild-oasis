98280daf6679952b980a4728e0d0e53c27c787cc
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-11-23 15:43:41 +0800

    added styled ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>The Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check In!")}>Check in</Button>
+        <Button onClick={() => alert("Check Out!")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
