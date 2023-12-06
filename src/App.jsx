c1c8e0bcae555de5a38cfa9936f39766c4feec73
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-12-06 22:53:27 +0800

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cabins" />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
